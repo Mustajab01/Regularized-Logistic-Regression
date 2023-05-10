313f9d7b49a1f2a9adbeca14e8e2042f4cdae4a1
Mustajab Ali <101339350+Mustajab01@users.noreply.github.com>
2023-05-10 15:50:12 +0500

    Added all files

---
+++ LogReg_Func.py
@@ -0,0 +1,174 @@
+import numpy as np
+import math
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Args:
+        z (ndarray): A scalar, numpy array of any size.
+
+    Returns:
+        g (ndarray): sigmoid(z), with the same shape as z
+         
+    """
+          
+    ez= np.exp(-z)
+    g = 1/(1+ ez)
+        
+    return g
+
+def compute_cost(X, y, w, b, *argv):
+    """
+    Computes the cost over all examples
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value 
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
+      *argv : unused, for compatibility with regularized version below
+    Returns:
+      total_cost : (scalar) cost 
+    """
+
+    m, n = X.shape
+    eps = 1e-9  #important to add, because otherwise if the denomenator becomes zero in the sigmoid function, it will cause an error.
+        
+    loss = 0.0
+    for i in range(m):
+        z = np.dot(X[i],w) + b 
+        loss += ((-y[i] * np.log(sigmoid(z) +eps)) - (1-y[i])*np.log(1-sigmoid(z) +eps))
+        
+    total_cost = loss/m
+    
+    return total_cost
+
+def compute_gradient(X, y, w, b, *argv): 
+    """
+    Computes the gradient for logistic regression 
+ 
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value 
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
+      *argv : unused, for compatibility with regularized version below
+    Returns
+      dj_dw : (ndarray Shape (n,)) The gradient of the cost w.r.t. the parameters w. 
+      dj_db : (scalar)             The gradient of the cost w.r.t. the parameter b. 
+    """
+    m, n = X.shape
+    dj_dw = np.zeros(w.shape)
+    dj_db = 0.
+    
+    inner_sum_db = 0
+    inner_sum_dw = 0
+    for i in range(m):
+        z = np.dot(X[i], w) + b
+        f_wb = sigmoid(z)
+        
+        inner_sum_db += (f_wb - y[i])
+        inner_sum_dw += (f_wb - y[i]) * X[i]
+        
+        
+    dj_db = inner_sum_db/m
+    dj_dw = inner_sum_dw/m
+        
+    return dj_db, dj_dw
+
+def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters, lambda_): 
+    """
+    Performs batch gradient descent to learn theta. Updates theta by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      X :    (ndarray Shape (m, n) data, m examples by n features
+      y :    (ndarray Shape (m,))  target value 
+      w_in : (ndarray Shape (n,))  Initial values of parameters of the model
+      b_in : (scalar)              Initial value of parameter of the model
+      cost_function :              function to compute cost
+      gradient_function :          function to compute gradient
+      alpha : (float)              Learning rate
+      num_iters : (int)            number of iterations to run gradient descent
+      lambda_ : (scalar, float)    regularization constant
+      
+    Returns:
+      w : (ndarray Shape (n,)) Updated values of parameters of the model after
+          running gradient descent
+      b : (scalar)                Updated value of parameter of the model after
+          running gradient descent
+    """
+    
+    # number of training examples
+    m = len(X)
+    
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w_history = []
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = gradient_function(X, y, w_in, b_in, lambda_)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w_in = w_in - alpha * dj_dw               
+        b_in = b_in - alpha * dj_db              
+       
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            cost =  cost_function(X, y, w_in, b_in, lambda_)
+            J_history.append(cost)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0 or i == (num_iters-1):
+            w_history.append(w_in)
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
+        
+    return w_in, b_in, J_history, w_history #return w and J,w history for graphing
+
+
+def predict(X, w, b): 
+    """
+    Predict whether the label is 0 or 1 using learned logistic
+    regression parameters w
+    
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
+
+    Returns:
+      p : (ndarray (m,)) The predictions for X using a threshold at 0.5
+    """
+    # number of training examples
+    m, n = X.shape   
+    p = np.zeros(m)
+   
+    # Loop over each example
+    for i in range(m):   
+        
+        # Calculate f_wb (exactly how you did it in the compute_cost function above)
+        z_wb = 0
+        # Loop over each feature
+        for j in range(n): 
+            
+            # Add the corresponding term to z_wb
+            z_wb_ij = X[i, j] * w[j]
+            z_wb += z_wb_ij
+
+        # Add bias term 
+        z_wb += b
+
+        # Calculate the prediction from the model
+        f_wb = sigmoid(z_wb)
+        
+
+        # Apply the threshold
+        if (f_wb >= .5):
+            p[i] = 1
+        
+        else:
+            p[i] = 0
+            
+    return p
